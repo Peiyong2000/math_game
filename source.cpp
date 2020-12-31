800adaf60934e569fb4507ca30fbb9fdf42379d2
Lo Peiyong <peiyong0359@gmail.com>
2020-12-31 18:25:14 +0800

    program with adder quiz

--- source.cpp
+++ source.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h> 
 using namespace std;
 
-int main(){
-    cout << "\n Hello World";
-    cout << "\n I like Git";
-    cout << "\n I like Github";
-	return 1;    
+int main() {
+	cout <<"\n Hello world";
+	srand (time(NULL)); // random seed
+	int ans;
+	int x = rand() % 10 + 1;
+	int y = rand() % 10 + 1;
+	cout << " \n Test your addition skill ";
+	printf("\n %d + %d = ?? ",x,y) ;
+	cin >> ans;
+	if (ans != (x+y) )
+		cout << "\n Wrong ";
+	else
+		cout << "\n Correct ";
+		
+	
+	return 1;
 }
